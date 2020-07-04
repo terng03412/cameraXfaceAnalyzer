0d55739a6d31d55e20ef2a51e242dbeedbd6d3e5
terng03412 <phattharaphon.r@ku.th>
2020-07-04 23:48:01 +0700

    add firebase

--- app/src/main/java/com/hpcnc/cameraxfaceanalyzer/MainActivity.kt
+++ app/src/main/java/com/hpcnc/cameraxfaceanalyzer/MainActivity.kt
@@ -3,8 +3,8 @@ package com.hpcnc.cameraxfaceanalyzer
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Matrix
+import android.graphics.*
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,10 +19,11 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
 import kotlinx.android.synthetic.main.activity_main.*
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
-import org.tensorflow.lite.nnapi.NnApiDelegate
 import org.tensorflow.lite.support.common.FileUtil
 import org.tensorflow.lite.support.common.ops.NormalizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
@@ -30,6 +31,7 @@ import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
 import org.tensorflow.lite.support.image.ops.Rot90Op
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.Executors
 import kotlin.random.Random
 
@@ -49,6 +51,22 @@ class MainActivity : AppCompatActivity() {
     private var imageRotationDegrees: Int = 0
     private val tfImageBuffer = TensorImage(DataType.UINT8)
 
+    private val metadata = FirebaseVisionImageMetadata.Builder()
+        .setWidth(640)
+        .setHeight(480)
+        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21 )
+        .setRotation(degreesToFirebaseRotation(90))
+        .build()
+
+    private fun degreesToFirebaseRotation(degrees: Int): Int = when(degrees) {
+        0 -> FirebaseVisionImageMetadata.ROTATION_0
+        90 -> FirebaseVisionImageMetadata.ROTATION_90
+        180 -> FirebaseVisionImageMetadata.ROTATION_180
+        270 -> FirebaseVisionImageMetadata.ROTATION_270
+        else -> throw Exception("Rotation must be 0, 90, 180, or 270.")
+    }
+
+
     private val tfImageProcessor by lazy {
         val cropSize = minOf(bitmapBuffer.width, bitmapBuffer.height)
         ImageProcessor.Builder()
@@ -65,7 +83,7 @@ class MainActivity : AppCompatActivity() {
     private val tflite by lazy {
         Interpreter(
             FileUtil.loadMappedFile(this, MODEL_PATH),
-            Interpreter.Options().addDelegate(NnApiDelegate()))
+            Interpreter.Options())
     }
 
 
@@ -119,17 +137,26 @@ class MainActivity : AppCompatActivity() {
                         image.width, image.height, Bitmap.Config.ARGB_8888)
                 }
 
+                val bitmap = toBitmap( image?.image!! )
+                val inputImage = FirebaseVisionImage.fromByteArray( BitmaptoNv21( bitmap ) , metadata )
+
+
+
                 // Early exit: image analysis is in paused state
                 if (pauseAnalysis) {
                     image.close()
                     return@Analyzer
                 }
 
+
+
                 // Convert the image to RGB and place it in our shared buffer
-//                image.use { converter.yuvToRgb(image.image!!, bitmapBuffer) }
+                image.use { converter.yuvToRgb(image.image!!, bitmapBuffer) }
+
+
 
                 // Process the image in Tensorflow
-                val tfImage =  tfImageProcessor.process(tfImageBuffer.apply { load(bitmapBuffer) })
+//                val tfImage =  tfImageProcessor.process(tfImageBuffer.apply { load(bitmapBuffer) })
 
 
             })
@@ -147,6 +174,63 @@ class MainActivity : AppCompatActivity() {
 
         }, ContextCompat.getMainExecutor(this))
     }
+
+    private fun BitmaptoNv21( bitmap: Bitmap ): ByteArray {
+        val argb = IntArray(bitmap.width * bitmap.height )
+        bitmap.getPixels(argb, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val yuv = ByteArray(bitmap.height * bitmap.width + 2 * Math.ceil(bitmap.height / 2.0).toInt()
+                * Math.ceil(bitmap.width / 2.0).toInt())
+        encodeYUV420SP( yuv, argb, bitmap.width, bitmap.height)
+        return yuv
+    }
+    private fun encodeYUV420SP(yuv420sp: ByteArray, argb: IntArray, width: Int, height: Int) {
+        val frameSize = width * height
+        var yIndex = 0
+        var uvIndex = frameSize
+        var R: Int
+        var G: Int
+        var B: Int
+        var Y: Int
+        var U: Int
+        var V: Int
+        var index = 0
+        for (j in 0 until height) {
+            for (i in 0 until width) {
+                R = argb[index] and 0xff0000 shr 16
+                G = argb[index] and 0xff00 shr 8
+                B = argb[index] and 0xff shr 0
+                Y = (66 * R + 129 * G + 25 * B + 128 shr 8) + 16
+                U = (-38 * R - 74 * G + 112 * B + 128 shr 8) + 128
+                V = (112 * R - 94 * G - 18 * B + 128 shr 8) + 128
+                yuv420sp[yIndex++] = (if (Y < 0) 0 else if (Y > 255) 255 else Y).toByte()
+                if (j % 2 == 0 && index % 2 == 0) {
+                    yuv420sp[uvIndex++] = (if (V < 0) 0 else if (V > 255) 255 else V).toByte()
+                    yuv420sp[uvIndex++] = (if (U < 0) 0 else if (U > 255) 255 else U).toByte()
+                }
+                index++
+            }
+        }
+    }
+
+
+    private fun toBitmap( image : Image): Bitmap {
+        val yBuffer = image.planes[0].buffer
+        val uBuffer = image.planes[1].buffer
+        val vBuffer = image.planes[2].buffer
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+        val nv21 = ByteArray(ySize + uSize + vSize)
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+        val yuv = out.toByteArray()
+        return BitmapFactory.decodeByteArray(yuv, 0, yuv.size)
+    }
+
 
 
     override fun onRequestPermissionsResult(
